3ba8e9bf97a6ef4da648476c2205824f1e99c17c
marianTestado <149865048+marianTestado@users.noreply.github.com>
2024-01-08 11:39:41 -0300

    Added color randomizer.

--- js/javascript.js
+++ js/javascript.js
@@ -11,6 +11,30 @@ let gridValue = document.querySelector('#gridValue');
 
 let painting = false;
 
+let colorEnabled = false;
+const colorBtn = document.querySelector('#color');
+
+
+    colorBtn.addEventListener('click', () =>{
+       if(colorEnabled)
+       {
+        colorBtn.style.backgroundColor = 'none';
+        colorBtn.style.color = 'black';
+        colorBtn.innerText = 'Color Mode';
+        colorBtn.style.backgroundImage = 'linear-gradient(lightgreen,yellow, orange, red, pink, violet,purple, blue, green)';       
+        colorEnabled = false;
+       }
+       else
+       {
+        colorBtn.style.backgroundColor = 'black';
+        colorBtn.style.color = 'white';
+        colorBtn.innerText = 'Black Mode';
+        colorBtn.style.backgroundImage = 'none';
+        colorEnabled = true;
+       }
+       
+    })
+
 gridRange.addEventListener('input',  () => {
     squareSize = gridRange.value;
     gridValue.innerText = `${squareSize}x${squareSize}`;
@@ -39,8 +63,15 @@ function createDiv(size){
     });
 
     div.addEventListener('mouseover', () => {
-        if (painting) {
+        if (painting && !colorEnabled) {
             div.style.backgroundColor = 'black';
+        }
+        else
+        {
+            if(painting && colorEnabled)
+            {
+                div.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255) + 1},${Math.floor(Math.random() * 255) + 1},${Math.floor(Math.random() * 255) + 1})`;
+            }
         }
     });
 
